52732ee4828df9a4bdb9e4a162cf69cf0a076c04
KyleJemino <kcjemino@up.edu.ph>
2022-10-25 00:10:10 +0800

    user binary id for migrations

--- priv/repo/migrations/20221024153107_add_todo_table.exs
+++ priv/repo/migrations/20221024153107_add_todo_table.exs
@@ -2,11 +2,12 @@ defmodule Doer.Repo.Migrations.AddTodoTable do
   use Ecto.Migration
 
   def change do
-    create table(:todos) do
+    create table(:todos, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :content, :string, null: false
       add :done_at, :utc_datetime
-      add :user_id, references(:users, on_delete: :nothing), null: false
-      add :todo_id, references(:todos, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :nothing, type: :binary_id), null: false
+      add :todo_id, references(:todos, on_delete: :nothing, type: :binary_id)
 
       timestamps()
     end
