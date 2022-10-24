70eebd36c5d40405b003e1f12f52d2e37faee790
KyleJemino <kcjemino@up.edu.ph>
2022-10-24 23:53:07 +0800

    Adds todo resolver

---
+++ lib/doer/todos/reducers/todo_resolver.ex
@@ -0,0 +1,19 @@
+defmodule Doer.Todos.Resolvers.TodoResolver do
+  alias Doer.Todos.Todo
+
+  def list_todo(_params \\ %{}) do
+    Repo.all(Todos)  
+  end
+
+  def get_todo(%{"id" => id} = params \\ %{}) do
+    Todo 
+    |> where([t], t.id == ^id)
+    |> Repo.one()
+  end
+
+  def create_todo(params \\ %{}) do
+    %Todo{}
+    |> Todo.changeset(params)
+    |> Repo.insert()
+  end
+end
